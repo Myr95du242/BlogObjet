b014a31e492f209e0868af936000c0600584b582
Myrna NZ <mnzabi.lan@gmail.com>
2021-05-19 23:17:14 +0200

    Utilisation objet controler

--- controler.php
+++ controler.php
@@ -2,34 +2,58 @@
 include('Blog.php');
 include('manager.php');
 
-$req_sql='INSERT INTO contenu(titre,date_contenu,commentaire,photo)
-					VALUES(?,NOW(),?,?) ';
-//prepare data
-$req=$bdd->prepare($req_sql);
+try{
 
-if(!empty($_POST['titre']) AND !empty($_POST['message']) ){
-	$titre=htmlspecialchars(addslashes($_POST['titre']));
-	$commentaire=htmlspecialchars(addslashes($_POST['message']));
-	$photo=$_FILES['photo']['name'];
+	//connexion à la bdd
+$base= new PDO('mysql:host=localhost;dbname=blogobjet;charset=utf8','root','',array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
-$saisi=array($titre,$commentaire,$photo);
+	if ($_FILES['photo']['error']){
+		switch ($_FILES['photo']['error']) 
+		{
+			case 1:
+				echo "La taille de votre fichier est supérieur à 2M <br/> ";
+				break;			
+			case 2:
+				echo "La taille de votre fichier est nulle <br/> ";
+				break;
+		}
+	}else{
+		echo "Aucune erreur avec le fichier <br/> ";
+		
+		if( (isset($_FILES['photo']['name'] ) && ($_FILES['photo']['error']== UPLOAD_ERR_OK) )){
+			$destination="postes/";
+			move_uploaded_file($_FILES['photo']['tmp_name'], $destination.$_FILES['photo']['name']);
+			echo 'Fichier copié dans le repertoire';
+		}else{
+			echo "Fichier pas connecté ! <br/> ";
+		}
+	}
+	if(!empty($_POST['titre']) AND !empty($_POST['message']) )
+	{
 
-//var_dump($saisi);
+		$manager= new Manager($base);
+		$titre=htmlspecialchars(addslashes($_POST['titre']));
+		$date=date("Y-m-d H:i:s");
+		$commentaire=htmlspecialchars(addslashes($_POST['message']));
+		$photo=$_FILES['photo']['name'];
 
-	$blog= new BLOG($titre,$commentaire,$photo);
+		$blog= new Blog($titre,$date,$commentaire,$photo);
 
-	// Récupération des données du blog
-	$titreSaisi=$blog->getTitre();
-	$commentaireSaisi=$blog->getCommentaire();
-	$photoSaisi=$blog->getPhoto();
-echo 'Votre enregistrement a réussi';
-	//Exécution de la requête
-	$data= $req->execute(array($titreSaisi,$commentaireSaisi,$photoSaisi) );
+		//Insertion avec la fonction 
+		$identifiant= $manager->insertionBdd($blog);
+		echo 'Votre enregistrement a réussi';
+		if ($identifiant != 0){
+			echo 'Ajout de vos données dans la bdd <br/>';
+		}else{
+			echo 'Les données ne sont pas enregistrées !';
+		}
+		echo '</br> Votre enregistrement a réussi! <br/>';
+	}
 
-	//echo 'Votre enregistrement a réussi';
-
+}catch(Exception $e) 
+{
+	die('Erreur :'.$e->getMessage() );
 }
-
 
 
 ?>
